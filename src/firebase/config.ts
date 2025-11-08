b9f7c2abe260a00515d3a99e4fb4094872a420ed
Saat Phere <saatphere25@gmail.com>
2025-11-08 08:53:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9081948496-42b9e",
+  "appId": "1:544220201811:web:530b49f2ce3d28ebc897a6",
+  "apiKey": "AIzaSyCiiLWOEomO81GwiusELoykFv6QxSOoCtM",
+  "authDomain": "studio-9081948496-42b9e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "544220201811"
+};
