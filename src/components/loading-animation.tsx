9bd1064314994d1b68aa6937cfcc770d6d6cf1b8
Saat Phere <saatphere25@gmail.com>
2025-11-10 06:16:10 +0000

    Still animation issue is not fixed

--- src/components/loading-animation.tsx
+++ src/components/loading-animation.tsx
@@ -14,7 +14,7 @@ export default function LoadingAnimation() {
       {icons.map((Icon, index) => (
         <Icon.component
           key={Icon.key}
-          className="absolute inset-0 w-full h-full text-accent animate-icon-fade"
+          className="absolute inset-0 w-full h-full text-accent opacity-0 animate-icon-fade"
           style={{ animationDelay: `${index * 1.5}s` }}
         />
       ))}
